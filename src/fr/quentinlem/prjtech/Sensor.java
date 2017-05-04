d08a33b5c89342a92ea6aa6926878cd225f879aa
Morpheus <quentin.lemonnier.info@gmail.com>
2017-05-04 20:18:46 +0200

    Sensor mistake fix

--- src/fr/quentinlem/prjtech/Sensor.java
+++ src/fr/quentinlem/prjtech/Sensor.java
@@ -77,7 +77,11 @@ public class Sensor {
     }
 
     public Message getLastMessage(){
-      return sensorTabMessages[(int)getSensorCurrentMsgId()-1%MESSAGE_TAB_SIZE];
+      if(getSensorCurrentMsgId == 0){
+        return sensorTabMessages[MESSAGE_TAB_SIZE-1];
+      } else {
+        return sensorTabMessages[(int)getSensorCurrentMsgId()-1%MESSAGE_TAB_SIZE];
+      }
     }
 
     public Message getMessageById(long id){
